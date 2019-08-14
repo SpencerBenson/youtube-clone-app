a33cc207b686c4cb2621f0929ac57de7b07d3791
Spencer Benson <benson.kariuki@andela.com>
2019-08-14 14:07:27 +0300

    created the searchbar component with Material-UI

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,38 @@
 import React, {Component} from 'react'
+import {Paper, TextField} from '@material-ui/core'
 
 class SearchBar extends Component {
+  state = {
+    searchTerm:''
+  }
+
+  handleChange = (e) => { 
+    this.setState({searchTerm: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const {searchTerm} = this.state
+    const {onFormSubmit} = this.props
+
+    onFormSubmit(searchTerm)
+  }
+  
   render () {
-    return (<h1>SearchBar Component</h1>)
+    const {searchTerm} = this.state
+    return (
+      <Paper elevation={6} style={{padding:'25px'}}>
+        <form onSubmit={this.handleSubmit}>
+          <TextField 
+            fullwidth 
+            label="Search..." 
+            value={searchTerm} 
+            onChange={this.handleChange} 
+          />
+        </form>
+      </Paper>
+    )
   }
 }
 export default SearchBar
